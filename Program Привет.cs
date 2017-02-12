1676cc9e25c05eaa9f0f057f43737407fe1d6adb
svetnt <svetnt@rambler.ru>
2017-02-12 13:45:05 +0500

    Add files via upload

---
+++ Program Привет.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, World!");
+            Console.ReadKey();
+        }
+    }
+}
